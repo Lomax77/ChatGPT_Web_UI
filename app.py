96b97eedb223934873f19930948e59895ae75ce8
TheLomaxProject <courtney.d.lomax@gmail.com>
2024-01-19 10:27:07 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, render_template #Flask is a micro web app framework written in Python.
+import openai
+
+app = Flask(__name__)
+
+openai.api_key = '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'
+
+@app.route('/', methods = ['GET']) #Get request to openai API
+def index():
+    return render_template('index.html')
+
+@app.route('/prompt', methods = ['POST']) #Post request to openai API
+def prompt():
+    data = request.get_json()
+    input_text = data['text'] #Gets the input text, saves it to 'input_text' variable, so that it can be sent to openai
+    
+    response = openai.ChatCompletion.create(
+         model = 'gpt-3.5-turbo',
+         messages = [{"role": "user", "content": input_text}]
+    ) #In the variable 'response', select your gpt model, and send your message with the role of user, and your content being 'input_text' form earlier
+    
+    generated_text = response.choices[0].message.content
+    
+    return generated_text
+
+if __name__ == '__main__':
+    app.run()
